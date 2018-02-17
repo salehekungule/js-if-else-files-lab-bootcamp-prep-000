94e083d7a1c2a691cc8caa4ce25e242e5f465dfb
Salehe Kungule <s.kungule@irdp.ac.tz>
2018-02-17 13:33:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,5 +9,5 @@ module.exports = { name, height
 if (name==="Susan"){
   console.log("The name is correct");
 }else {
-  console.log("Expected the name tone Susan");
+  console.log("Expected the name to be Susan");
 }
